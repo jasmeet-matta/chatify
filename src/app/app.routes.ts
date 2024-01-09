627a435aa35c2582e673d20d56167f873d52a9bb
Jasmeet Matta <jashoney99@gmail.com>
2024-01-09 09:24:28 +0000

    default component created and roututed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DefaultComponent } from './components/default/default.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: DefaultComponent }, // Setting default route
+];

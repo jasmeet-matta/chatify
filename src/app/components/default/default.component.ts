ad649b954457578d406bac14f533f52df9540a62
Jasmeet <jashoney99@gmail.com>
2024-01-12 01:41:45 +0530

    emoji panel toggle & custom import

--- src/app/components/default/default.component.ts
+++ src/app/components/default/default.component.ts
@@ -1,24 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, CUSTOM_ELEMENTS_SCHEMA, } from '@angular/core';
 
 @Component({
   selector: 'app-default',
   standalone: true,
   imports: [],
   templateUrl: './default.component.html',
-  styleUrl: './default.component.scss'
+  styleUrl: './default.component.scss',
+  schemas: [
+    CUSTOM_ELEMENTS_SCHEMA
+  ],
 })
 export class DefaultComponent implements OnInit {
 
   public title:string = 'Chatify';
+  public inputPlaceholder:string = 'type your message here...'
   public chatRoom:string = '/assets/chat-room.png';
   public menu :string = '/assets/menu.png';
   public send:string = 'assets/send.png';
   public smiley:string = 'assets/smiley.png';
+  public showEmojis:boolean = false;
 
   constructor(){ }
 
-  ngOnInit() {
-    
+  ngOnInit() { }
+
+  toggleEmojiDrawer(){
+    this.showEmojis = !this.showEmojis
+  }
+
+  getEmoji(event:any){
+    console.log(event);
   }
 
 }
